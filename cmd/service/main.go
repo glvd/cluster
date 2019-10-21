9b15528eda8670a5b57a980335439a853be193c0
godcong <jumbycc@163.com>
2019-10-21 14:57:18 +0800

    add cluster

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/godcong/go-trait"
+import (
+	"github.com/blang/semver"
+	"github.com/godcong/go-trait"
+)
 
 var log = trait.NewZapSugar()
 
@@ -22,6 +25,27 @@ var (
 	identityPath string
 )
 
+func init() {
+	// Set build information.
+	if build, err := semver.NewBuildVersion(commit); err == nil {
+		version.Version.Build = []string{"git" + build}
+	}
+
+	// We try guessing user's home from the HOME variable. This
+	// allows HOME hacks for things like Snapcraft builds. HOME
+	// should be set in all UNIX by the OS. Alternatively, we fall back to
+	// usr.HomeDir (which should work on Windows etc.).
+	home := os.Getenv("HOME")
+	if home == "" {
+		usr, err := user.Current()
+		if err != nil {
+			panic(fmt.Sprintf("cannot get current user: %s", err))
+		}
+		home = usr.HomeDir
+	}
+
+	DefaultPath = filepath.Join(home, DefaultFolder)
+}
 func main() {
 
 }
